c5b7ada5fca65f6080720be4ee5065348e721217
Obafemi Teminife <andysakov1958@gmail.com>
2021-03-06 13:57:55 +0100

    Update DB.scala

--- src/main/scala/com/hex/db/DB.scala
+++ src/main/scala/com/hex/db/DB.scala
@@ -3,50 +3,20 @@ package com.hex.db
 import java.sql.{Connection, DriverManager, Statement}
 
 trait DB {
-  protected var timeout: Int = 1000
-  protected var driver: String= ""
-  protected var url: String = ""
-  protected var user: String = ""
-  protected var pass: String = ""
-  protected var db: String = ""
+  protected def timeout: Int = 1000
+  protected def driver: String
+  protected def url: String
+  protected def user: String
+  protected def pass: String
+  protected def db: String
   
   Class.forName(driver)
-  lazy val conn: Connection = DriverManager.getConnection(url, user, pass)
-  lazy val state: Statement = conn.createStatement()
+  private lazy val conn: Connection = DriverManager.getConnection(url, user, pass)
+  private lazy val state: Statement = conn.createStatement()
 
   def connect(): Boolean = {
     state.execute(s"SELECT * FROM $db")
     conn.isValid(timeout)
-  }
-
-  def setTimeout(int: Int): this.type ={
-    this.timeout = int
-    this
-  }
-  
-  def setDriver(driver: String): this.type = {
-    this.driver = driver
-    this
-  }
-  
-  def setUrl(url: String): this.type = {
-    this.url = url
-    this
-  }
-  
-  def setUser(user: String): this.type = {
-    this.user = user
-    this
-  }
-  
-  def setPass(pass: String): this.type = {
-    this.pass = pass
-    this
-  }
-  
-  def setDb(db: String): this.type = {
-    this.db = db
-    this
   }
 
 }
